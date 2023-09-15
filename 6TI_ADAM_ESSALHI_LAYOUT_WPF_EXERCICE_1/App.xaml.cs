8e6d6f321756160a1591587b7a28a9d43e0c164c
Essalhi Adam <200537@CCM-Asty-Moulin.local>
2023-09-15 13:55:48 +0200

    layout

---
+++ 6TI_ADAM_ESSALHI_LAYOUT_WPF_EXERCICE_1/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _6TI_ADAM_ESSALHI_LAYOUT_WPF_EXERCICE_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
